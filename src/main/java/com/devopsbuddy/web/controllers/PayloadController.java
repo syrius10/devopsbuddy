7bd3748d56fe4fcbdd523bd5917a81e4a6c56801
Victor Stanculescu <stanculescu.victor@gmail.com>
2017-06-27 01:01:11 +0100

    Created a payload page

---
+++ src/main/java/com/devopsbuddy/web/controllers/PayloadController.java
@@ -0,0 +1,19 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Syrius on 6/27/2017.
+ */
+@Controller
+public class PayloadController {
+
+    public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload() {
+        return PAYLOAD_VIEW_NAME;
+    }
+
+}
