485a296c0d3c96c43cd923c46e5bf37f11fc18f8
Victor Stanculescu <stanculescu.victor@gmail.com>
2017-07-09 22:14:14 +0100

    Created the frontend domain model and translation layer for the basic account

--- src/main/java/com/devopsbuddy/utils/UserUtils.java
+++ src/main/java/com/devopsbuddy/utils/UserUtils.java
@@ -2,6 +2,7 @@ package com.devopsbuddy.utils;
 
 import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -63,5 +64,20 @@ public class UserUtils {
 
         return passwordResetUrl;
 
+    }
+
+    public static <T extends BasicAccountPayload> User fromWebUserToDomainUser(T frontendPayload) {
+        User user = new User();
+        user.setUsername(frontendPayload.getUsername());
+        user.setPassword(frontendPayload.getPassword());
+        user.setFirstName(frontendPayload.getFirstName());
+        user.setLastName(frontendPayload.getLastName());
+        user.setEmail(frontendPayload.getEmail());
+        user.setPhoneNumber(frontendPayload.getPhoneNumber());
+        user.setCountry(frontendPayload.getCountry());
+        user.setEnabled(true);
+        user.setDescription(frontendPayload.getDescription());
+
+        return user;
     }
 }
--- src/test/java/com/devopsbuddy/test/unit/UserUtilsUnitTest.java
+++ src/test/java/com/devopsbuddy/test/unit/UserUtilsUnitTest.java
@@ -1,12 +1,19 @@
 package com.devopsbuddy.test.unit;
 
+import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.utils.UserUtils;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
+import uk.co.jemos.podam.api.ClassInfoStrategy;
+import uk.co.jemos.podam.api.DataProviderStrategy;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
+import java.lang.reflect.Type;
 import java.util.UUID;
 
 /**
@@ -16,8 +23,13 @@ public class UserUtilsUnitTest {
 
     private MockHttpServletRequest mockHttpServletRequest;
 
+    private PodamFactory podamFactory;
+
     @Before
-    public void init() { mockHttpServletRequest = new MockHttpServletRequest(); }
+    public void init() {
+        mockHttpServletRequest = new MockHttpServletRequest();
+        podamFactory = new PodamFactoryImpl();
+    }
 
     @Test
     public void testPasswordResetEmailUrlConstruction() throws Exception {
@@ -33,6 +45,26 @@ public class UserUtilsUnitTest {
         String actualUrl = UserUtils.createPasswordResetUrl(mockHttpServletRequest, userId, token);
 
         Assert.assertEquals(expectedUrl, actualUrl);
+
+    }
+
+    @Test
+    public void mapWebUserToDomainUser() {
+
+        BasicAccountPayload webUser = podamFactory.manufacturePojoWithFullData(BasicAccountPayload.class);
+        webUser.setEmail("me@example.com");
+
+        User user = UserUtils.fromWebUserToDomainUser(webUser);
+        Assert.assertNotNull(user);
+
+        Assert.assertEquals(webUser.getUsername(), user.getUsername());
+        Assert.assertEquals(webUser.getPassword(), user.getPassword());
+        Assert.assertEquals(webUser.getFirstName(), user.getFirstName());
+        Assert.assertEquals(webUser.getLastName(), user.getLastName());
+        Assert.assertEquals(webUser.getEmail(), user.getEmail());
+        Assert.assertEquals(webUser.getPhoneNumber(), user.getPhoneNumber());
+        Assert.assertEquals(webUser.getCountry(), user.getCountry());
+        Assert.assertEquals(webUser.getDescription(), user.getDescription());
 
     }
 
