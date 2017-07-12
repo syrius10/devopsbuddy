debf5478b5349f9c3f3c0fc6d2480d582709ea73
Victor Stanculescu <stanculescu.victor@gmail.com>
2017-07-12 18:31:22 +0100

    Added image upload capability

--- src/main/java/com/devopsbuddy/web/controllers/SignupController.java
+++ src/main/java/com/devopsbuddy/web/controllers/SignupController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -73,6 +74,7 @@ public class SignupController {
 
     @RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.POST)
     public String signUpPost(@RequestParam(name = "planId", required = true) int planId,
+                             @RequestParam(name = "file", required = false) MultipartFile file,
                              @ModelAttribute(PAYLOAD_MODEL_KEY_NAME) @Valid ProAccountPayload payload,
                              ModelMap model) throws IOException {
 
@@ -110,6 +112,19 @@ public class SignupController {
         // There are certain info that the user doesn't set, such as profile image URL, Stripe customer id, plans and roles
         LOG.debug("Transforming user payload into User domain object");
         User user = UserUtils.fromWebUserToDomainUser(payload);
+
+        // Stores the profile image on Amazon S3 and stores the URL in the user's record
+        if (file != null && !file.isEmpty()) {
+
+            String profileImageUrl = null;
+            if (profileImageUrl != null) {
+                user.setProfileImageUrl(profileImageUrl);
+            } else {
+                LOG.warn("There was a problem uploading the profile image S3. The user's profile will " +
+                        " be created without an image");
+            }
+
+        }
 
         // Sets the Plan and the Roles (depending on the chosen plan)
         LOG.debug("Retrieving plan from the database");
